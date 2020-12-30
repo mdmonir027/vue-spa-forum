7399c65398fa84f64374a1e3994a79ab4993101a
Md Monir <60777309+mdmonir27@users.noreply.github.com>
2020-12-31 01:00:42 +0600

    JWT Package Install and Setup

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,28 @@
 
 
 use App\Http\Controllers\Api\LikeController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('/question' , \App\Http\Controllers\Api\QuestionController::class);
-Route::apiResource('/category' , \App\Http\Controllers\Api\CategoryController::class);
-Route::apiResource('/question/{question}/reply' , \App\Http\Controllers\Api\ReplyController::class);
+Route::apiResource('/question', \App\Http\Controllers\Api\QuestionController::class);
+Route::apiResource('/category', \App\Http\Controllers\Api\CategoryController::class);
+Route::apiResource('/question/{question}/reply', \App\Http\Controllers\Api\ReplyController::class);
 
-Route::post('like/{reply}' , [LikeController::class , 'likeIt']);
-Route::delete('like/{reply}' , [LikeController::class , 'unlikeIt']);
+Route::post('like/{reply}', [LikeController::class, 'likeIt']);
+Route::delete('like/{reply}', [LikeController::class, 'unlikeIt']);
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
